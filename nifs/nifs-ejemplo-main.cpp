580dc0b0092e2bf59860e36e514fc7496c1d7a7b
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-11-10 20:28:09 +0100

    Update nifs-ejemplo-main.cpp

--- nifs/nifs-ejemplo-main.cpp
+++ nifs/nifs-ejemplo-main.cpp
@@ -1,11 +1,10 @@
-/********************************************************************************\
+/******************************************************************************\
  * Curso de Programación 1. Tema 11 (Registros)
  * Autores: Miguel Ángel Latre
  * Última revisión: 14 de noviembre de 2019
  * Resumen: Programa de ejemplo de uso de los recursos definidos en el módulo
  *          «nif»
- * Codificación de caracteres original de este fichero: UTF-8 sin BOM
-\********************************************************************************/
+\******************************************************************************/
 
 #include <iostream>
 #include "nif.hpp"
